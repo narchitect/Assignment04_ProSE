11cdefbc8b5e6c09e6687d54b29d1f82752ae78b
Nayun Kim <ny.kim@tum.de>
2023-01-12 12:21:10 +0100

    Make the base
    make the classes and attributes

---
+++ Assignment04_ProSE/Kitty.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Assignment04_ProSE
+{
+	public class Kitty
+	{
+		public int Id { get; set; }
+        public string Link { get; set; } = null!; //need to check data type of link
+        public string EventName { get; set; } = null!;
+        public Currency Currency { get; set; }
+        public int? GroupCost { get; set; }
+		public ICollection<Participant> Pariticipants { get; set; } = null!;
+        public ICollection<Participant>? WhoSeen { get; set; } = null!;
+        public ICollection<Participant>? SettledDebts { get; set; }
+        public ICollection<Comment>? Comments;
+
+        public Kitty()
+		{
+		}
+	}
+}
+
