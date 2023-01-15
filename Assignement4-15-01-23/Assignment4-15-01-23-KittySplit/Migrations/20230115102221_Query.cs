3b80e782f8021de2712d4c73686191af5d7d467a
tamira12081999 <tamira.wrabel@tum.de>
2023-01-15 18:16:08 +0100

    Assignment Methods

---
+++ Assignement4-15-01-23/Assignment4-15-01-23-KittySplit/Migrations/20230115102221_Query.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Assignment4150123KittySplit.Migrations
+{
+    /// <inheritdoc />
+    public partial class Query : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Assignement4-15-01-23/Assignment4-15-01-23-KittySplit/Payment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4_15_01_23_KittySplit
+{
+    public class Payment
+    {
+        public int Id { get; set; }
+        public string Purpose { get; set; } = null!;
+        public double Amount { get; set; }
+        public DateTime DateTime { get; set; }
+
+        public int ParticipantId { get; set; }
+        public Participant Participant { get; set; } = null!;
+    }
+}
